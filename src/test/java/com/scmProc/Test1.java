708fd7ddb6516c2f9436c27868be12456c64b7bc
VaishnaviKadam95 <vaishnavikadam01@gmail.com>
2024-01-12 15:10:51 +0530

    FrameWork is done

---
+++ src/test/java/com/scmProc/Test1.java
@@ -0,0 +1,11 @@
+package com.scmProc;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+	@Test
+	public void vaishnavi() {
+		System.out.println("Your salary is deposited in your account ");
+	}
+}
