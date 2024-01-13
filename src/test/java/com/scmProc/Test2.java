525babcd2cc2dc5f021801c60cc2d84aa04c0264
VaishnaviKadam95 <vaishnavikadam01@gmail.com>
2024-01-14 04:27:05 +0530

    Changes done by vaishnavi

---
+++ src/test/java/com/scmProc/Test2.java
@@ -0,0 +1,10 @@
+package com.scmProc;
+
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+public void vaishnavi1() {
+	System.out.println("Congratulations you got an increament in salary");
+}
+}
