7dd439088e2b1fa2fa7c98695d6858a55fdedba5
VaishnaviKadam95 <vaishnavikadam01@gmail.com>
2024-01-14 05:14:13 +0530

    CHANGES DONE

--- src/test/java/com/scmProc/Test2.java
+++ src/test/java/com/scmProc/Test2.java
@@ -5,6 +5,8 @@ import org.testng.annotations.Test;
 public class Test2 {
 	@Test
 public void vaishnavi1() {
+		String BROWSER=System.getProperty("browser");
+		String ENV=System.getProperty("url");
 	System.out.println("Congratulations you got an increament in salary ");
 }
 }
